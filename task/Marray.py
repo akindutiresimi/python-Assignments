58d9fda4b72ecf17c54228a87eb1c8e07d5c9371
Akindutire Simi <akindutiresimi@gmail.com>
2026-08-17 02:09:41 +0100

    Python_Assignment

---
+++ task/Marray.py
@@ -0,0 +1,39 @@
+
+
+
+myLists = [10.2, 25.6, 8.7, 15.6]
+max_value = myLists[0]
+index_max = 0
+
+for array in range(1, len(myLists)):
+    if myLists[array] > max_value:
+        max_value = myLists
+        index_max = array
+print(index_max)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
